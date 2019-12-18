08d93c50239ae954891c77b7fd0ac1a8f23335d1
Asia <asia.dobrowolska95@gmail.com>
2019-12-18 18:20:30 +0100

    create route components ErrorData & ErrorPage & LoadingData & WelcomePage

---
+++ src/components/route-components/LoadingData.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Loader = () => {
+  return (
+    <section className="section-loader">
+      <h3 className="section-loader__title">loading...</h3>
+    </section>
+  );
+};
+
+export default Loader;
---
+++ src/components/route-components/WelcomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WelcomePage = () => {
+  return (
+    <section className="section-welcome">
+      <h2 className="section-welcome__title">welcome to the shop</h2>
+      <h3 className="section-welcome__subtitle">
+        we invite you to check our product offer
+      </h3>
+    </section>
+  );
+};
+
+export default WelcomePage;
