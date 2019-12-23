f7dbbb65de6bad3f162b44eb189c35d57005ade6
Asia <asia.dobrowolska95@gmail.com>
2019-12-23 09:56:25 +0100

    Updates

---
+++ precache-manifest.a9c934362992a414fa46784d45719fee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d12815d64faa83acdaaea0dbcad84b4",
+    "url": "/electronics-world/index.html"
+  },
+  {
+    "revision": "3eec65b6c2e8969d9dc3",
+    "url": "/electronics-world/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1695c159a20a1a0058b9",
+    "url": "/electronics-world/static/css/main.86e53f44.chunk.css"
+  },
+  {
+    "revision": "3eec65b6c2e8969d9dc3",
+    "url": "/electronics-world/static/js/2.56299713.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/electronics-world/static/js/2.56299713.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1695c159a20a1a0058b9",
+    "url": "/electronics-world/static/js/main.a524fd5a.chunk.js"
+  },
+  {
+    "revision": "27c2670939a888f6a8bb",
+    "url": "/electronics-world/static/js/runtime-main.62f670c4.js"
+  },
+  {
+    "revision": "f03a751801165e1b2d8c4efbe35f8156",
+    "url": "/electronics-world/static/media/welcome.f03a7518.jpg"
+  }
+]);
